6b7ad5bb7b74c0f34c8f0ea1cba1e70529c8671e
Roberto <robertomerani@gmail.com>
2024-02-13 23:52:10 +0100

    C8 - refactoring the projects section and adding the Modal

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ closeSidebar.addEventListener('click', function () {
   links.classList.add('side-bar');
 });
 
-console.log(window.innerWidth);
+// console.log(window.innerWidth);
 
 window.addEventListener('resize', function () {
   if (window.innerWidth < 768) {
@@ -29,4 +29,52 @@ window.addEventListener('resize', function () {
     links.classList.remove('side-bar');
     // links.classList.add('close-sideBar');
   }
+});
+
+// modal
+
+const modal = document.querySelectorAll('.modal');
+const openModal = document.querySelector('.open-modal');
+const openContainer = document.querySelector('.open-container');
+
+const prevBtn = document.querySelector('.btn-prev');
+const nextBtn = document.querySelector('.btn-next');
+
+openModal.addEventListener('click', function () {
+  this.style.display = 'none';
+  openContainer.style.display = 'none';
+
+  document.body.classList.remove('modal-active');
+});
+
+modal.forEach(function (btn, index) {
+  btn.addEventListener('click', function () {
+    openModal.style.display = 'block';
+    openContainer.style.display = 'block';
+
+    document.body.classList.add('modal-active');
+
+    currentImageIdx = index + 1;
+    currentImageDisplay(currentImageIdx);
+  });
+});
+
+function currentImageDisplay() {
+  openContainer.style.background = `url(./assets/img/services-${currentImageIdx}.jpg) center/cover no-repeat`;
+}
+
+prevBtn.addEventListener('click', function () {
+  currentImageIdx--;
+  if (currentImageIdx === 0) {
+    currentImageIdx = modal.length;
+  }
+  currentImageDisplay(currentImageIdx);
+});
+
+nextBtn.addEventListener('click', function () {
+  currentImageIdx++;
+  if (currentImageIdx === 7) {
+    currentImageIdx = 1;
+  }
+  currentImageDisplay(currentImageIdx);
 });
